f0afdd5873018ff48b34d07cd344de3e2ca468cf
PimiYvan <ynjomkam@gmail.com>
2024-08-01 06:51:37 +0100

    small update

--- data/preprocess_isic.py
+++ data/preprocess_isic.py
@@ -2,15 +2,39 @@ import csv
 import os
 from shutil import copy
 
-ImgPath = '../dataset/ISIC/ISIC2018_Task1-2_Training_Input'
-AnnPath = '../dataset/ISIC/ISIC2018_Task1_Training_GroundTruth'
+ImgPath = '../dataset/ISIC-TMP/ISIC2018_Task1-2_Training_Input'
+AnnPath = '../dataset/ISIC-TMP/ISIC2018_Task1_Training_GroundTruth'
 
 
-with open('./isic/class_id.csv', encoding='utf-8-sig') as f:
+# with open('./isic/class_id.csv', encoding='utf-8-sig') as f:
+#     for row in csv.reader(f, skipinitialspace=True):
+#         if row[0] != 'ID':
+#             imgpath = os.path.join(ImgPath, row[0]) + '.jpg'
+#             annpath = os.path.join(AnnPath, row[0]) + '_segmentation.png'
+#             if row[1] == 'nevus':
+#                 copy(imgpath, os.path.join(ImgPath, '1', os.path.basename(imgpath)))
+#                 copy(imgpath, os.path.join(AnnPath, '1', os.path.basename(annpath)))
+#             elif row[1] == 'seborrheic_keratosis':
+#                 copy(imgpath, os.path.join(ImgPath, '2', os.path.basename(imgpath)))
+#                 copy(imgpath, os.path.join(AnnPath, '2', os.path.basename(annpath)))
+#             if row[1] == 'melanoma':
+#                 copy(imgpath, os.path.join(ImgPath, '3', os.path.basename(imgpath)))
+#                 copy(imgpath, os.path.join(AnnPath, '3', os.path.basename(annpath)))
+
+
+for i in range(1, 4):
+    print(i)
+    img_folder = os.path.join(ImgPath, str(i))
+    ann_folder = os.path.join(AnnPath, str(i))
+    os.makedirs(img_folder, exist_ok=True)
+    os.makedirs(ann_folder, exist_ok=True)
+
+with open('./data/isic/class_id.csv', encoding='utf-8-sig') as f:
     for row in csv.reader(f, skipinitialspace=True):
         if row[0] != 'ID':
             imgpath = os.path.join(ImgPath, row[0]) + '.jpg'
             annpath = os.path.join(AnnPath, row[0]) + '_segmentation.png'
+
             if row[1] == 'nevus':
                 copy(imgpath, os.path.join(ImgPath, '1', os.path.basename(imgpath)))
                 copy(imgpath, os.path.join(AnnPath, '1', os.path.basename(annpath)))

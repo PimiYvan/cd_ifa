5bcf407c728dc217953439a2681801a744fa4e28
PimiYvan <ynjomkam@gmail.com>
2024-08-03 16:29:19 +0100

    small update

--- data/preprocess_isic.py
+++ data/preprocess_isic.py
@@ -2,8 +2,11 @@ import csv
 import os
 from shutil import copy
 
-ImgPath = '../dataset/ISIC-TMP/ISIC2018_Task1-2_Training_Input'
-AnnPath = '../dataset/ISIC-TMP/ISIC2018_Task1_Training_GroundTruth'
+ImgPath = './dataset/ISIC-TMP/ISIC2018_Task1-2_Training_Input'
+AnnPath = './dataset/ISIC-TMP/ISIC2018_Task1_Training_GroundTruth'
+
+ImgPathTarget = './dataset/ISIC/ISIC2018_Task1-2_Training_Input'
+AnnPathTarget = './dataset/ISIC/ISIC2018_Task1_Training_GroundTruth'
 
 
 # with open('./isic/class_id.csv', encoding='utf-8-sig') as f:
@@ -26,8 +29,15 @@ for i in range(1, 4):
     print(i)
     img_folder = os.path.join(ImgPath, str(i))
     ann_folder = os.path.join(AnnPath, str(i))
+
+    img_target_folder = os.path.join(ImgPathTarget, str(i))
+    ann_target_folder = os.path.join(AnnPathTarget, str(i))
+
     os.makedirs(img_folder, exist_ok=True)
     os.makedirs(ann_folder, exist_ok=True)
+
+    os.makedirs(img_target_folder, exist_ok=True)
+    os.makedirs(ann_target_folder, exist_ok=True)
 
 with open('./data/isic/class_id.csv', encoding='utf-8-sig') as f:
     for row in csv.reader(f, skipinitialspace=True):
@@ -36,11 +46,13 @@ with open('./data/isic/class_id.csv', encoding='utf-8-sig') as f:
             annpath = os.path.join(AnnPath, row[0]) + '_segmentation.png'
 
             if row[1] == 'nevus':
-                copy(imgpath, os.path.join(ImgPath, '1', os.path.basename(imgpath)))
-                copy(imgpath, os.path.join(AnnPath, '1', os.path.basename(annpath)))
+                copy(imgpath, os.path.join(ImgPathTarget, '1', os.path.basename(imgpath)))
+                copy(imgpath, os.path.join(AnnPathTarget, '1', os.path.basename(annpath)))
             elif row[1] == 'seborrheic_keratosis':
-                copy(imgpath, os.path.join(ImgPath, '2', os.path.basename(imgpath)))
-                copy(imgpath, os.path.join(AnnPath, '2', os.path.basename(annpath)))
+                copy(imgpath, os.path.join(ImgPathTarget, '2', os.path.basename(imgpath)))
+                copy(imgpath, os.path.join(AnnPathTarget, '2', os.path.basename(annpath)))
             if row[1] == 'melanoma':
-                copy(imgpath, os.path.join(ImgPath, '3', os.path.basename(imgpath)))
-                copy(imgpath, os.path.join(AnnPath, '3', os.path.basename(annpath)))
+                copy(imgpath, os.path.join(ImgPathTarget, '3', os.path.basename(imgpath)))
+                copy(imgpath, os.path.join(AnnPathTarget, '3', os.path.basename(annpath)))
+
+    

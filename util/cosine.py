a09ad557de19c0bf889474e0e87c5695843883d0
PimiYvan <ynjomkam@gmail.com>
2024-11-07 18:09:06 +0100

    small update

--- util/cosine.py
+++ util/cosine.py
@@ -4,7 +4,7 @@ import torchvision
 from PIL import Image
 from torch import nn
 from torchvision import transforms as tr
-from torchvision.models import vit_h_14, vit_h_16
+from torchvision.models import vit_h_14, vit_l_16
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 
@@ -14,8 +14,8 @@ class cosineSimilarity:
     
     def __init__(self, device=None):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        # self.model = self.get_model()
-        self.model = self.get_model_v_16()
+        self.model = self.get_model()
+        # self.model = self.get_model_v_16()
         # self.image_path_1 = image_path_1
         # self.image_path_2 = image_path_2
     
@@ -49,8 +49,8 @@ class cosineSimilarity:
         Vision Transformer model object
 
         """
-        wt = torchvision.models.ViT_H_16_Weights.DEFAULT
-        model = vit_h_16(weights=wt)
+        wt = torchvision.models.ViT_L_16_Weights.DEFAULT
+        model = vit_l_16(weights=wt)
         
         # Remove the last classification layer (for transfer learning or feature extraction)
         model.heads = nn.Sequential(*list(model.heads.children())[:-1])

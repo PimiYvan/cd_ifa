3e863bb34d1189bc8844e6a88e3064afbca9a803
PimiYvan <ynjomkam@gmail.com>
2025-04-15 10:54:39 +0100

    changes in distractor fss file

--- data/fss_dist.py
+++ data/fss_dist.py
@@ -33,7 +33,7 @@ class DatasetFSSDist(Dataset):
 
     def __getitem__(self, idx):
         # query_name, support_names, class_sample = self.sample_episode(idx)
-        query_name, support_names, class_sample = self.sample_episode_with_distractor(idx, 5)
+        query_name, support_names, class_sample = self.sample_episode_with_distractor(idx, 1)
 
         query_img, query_mask, support_imgs, support_masks = self.load_frame(query_name, support_names)
 
